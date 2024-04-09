1817656946762a7c2e159e30f1d047381a26b9a4
Thaís <thcba@cesar.school>
2024-04-09 14:58:36 -0300

    ajuste em biblotecas

--- projeto/apps/views.py
+++ projeto/apps/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render # , redirect, get_object_or_404
-# from .models import *
-# from django.http import HttpResponse, HttpResponseRedirect
+from .models import *
+from django.http import HttpResponse, HttpResponseRedirect
 # from django.contrib import messages
 # from django.contrib.auth import authenticate, login, logout
 # from django.contrib.auth.decorators import login_required
