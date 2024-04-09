4aff03d56a368263c317abdd015238c401c9a1ba
Thaís <155742254+aguiarth@users.noreply.github.com>
2024-04-09 14:51:39 -0300

    template base

--- projeto/apps/urls.py
+++ projeto/apps/urls.py
@@ -1,7 +1,7 @@
 # from django.urls import path
 # from django.contrib.auth import views as auth_views
-# from . import views
+from . import views
 
 urlpatterns = [
-    # path('', views.home, name = 'home'),
+    path('', views.home, name = 'home'),
 ]
