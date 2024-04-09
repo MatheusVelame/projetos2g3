f5c3916f8db91e032585940366d9beebaa9316a3
Thaís <thcba@cesar.school>
2024-04-09 14:24:32 -0300

    arquivos iniciais

---
+++ projeto/apps/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render # , redirect, get_object_or_404
+# from .models import *
+# from django.http import HttpResponse, HttpResponseRedirect
+# from django.contrib import messages
+# from django.contrib.auth import authenticate, login, logout
+# from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.models import User
+# from django.urls import reverse
+
+# Create your views here.
+
+
