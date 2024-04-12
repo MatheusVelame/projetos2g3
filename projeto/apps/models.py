4ed4a615b1091f3e35783e5588ecff1af62ad87f
Thaís <thcba@cesar.school>
2024-04-12 20:09:41 -0300

    favoritos: models, views, url, template

--- projeto/apps/models.py
+++ projeto/apps/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# quem tiver com essa ponta de cadastro das cafeterias pode elaborar o model abaixo, só tô colocando um pontapé pra conseguir fazer o model "Favorito"
+class Cafe(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.nome
+    
+class Favorito(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    cafe = models.ForeignKey(Cafe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.usuario.username} - {self.cafe.nome}'
--- projeto/apps/urls.py
+++ projeto/apps/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
+    path('favoritos/', views.lista_favoritos, name='favoritos'),
+    path('favoritar/<int:cafe_id>', views.favoritar, name='favoritar'),
     path('login/', auth_views.LoginView.as_view(template_name='apps/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
