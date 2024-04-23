aef7c9652448b700f3e7bcdcff1664457266cc5c
Thaís <155742254+aguiarth@users.noreply.github.com>
2024-04-23 12:21:19 -0300

    página de detalhes

--- projeto/apps/urls.py
+++ projeto/apps/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('cafeteria/<int:cafe_id>/enviar-email/', enviar_email, name='enviar-email'),
     path('cafeteria/<int:cafe_id>/', perfil_cafeteria, name='perfil-cafeteria'),
     path('cafeteria/<int:cafe_id>/whatsapp/', enviar_whatsapp, name='enviar_whatsapp'),
-    path('detalhes/<int:espaco_id>/', views.detalhes, name='detalhes'),
+    path('detalhes/<int:cafe_id>/', views.detalhes, name='detalhes'),
     path('favoritos/', views.lista_favoritos, name='favoritos'),
     path('favoritar/<int:cafe_id>', views.favoritar, name='favoritar'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
