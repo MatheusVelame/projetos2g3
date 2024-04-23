9a66bed5ba337bee37b325090615d2be6463f76c
Thaís <thcba@cesar.school>
2024-04-23 14:07:20 -0300

    ajustes login

--- projeto/apps/urls.py
+++ projeto/apps/urls.py
@@ -6,7 +6,7 @@ from .views import *
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('cadastro_cafeteria/', views.cadastro_cafeteria, name='cadastro_cafeteria'),
-    path('cadastro_cliente/',views.user_cadastro, name='user_cadastro'),
+    path('cadastro_cliente/',views.cadastro_cliente, name='cadastro_cliente'),
     path('cafeteria/<int:cafe_id>/enviar-email/', enviar_email, name='enviar-email'),
     path('cafeteria/<int:cafe_id>/', perfil_cafeteria, name='perfil-cafeteria'),
     path('cafeteria/<int:cafe_id>/whatsapp/', enviar_whatsapp, name='enviar_whatsapp'),
