ffa7cde56fd00829cc049a94c7fb649a00ffe22f
Thaís <thcba@cesar.school>
2024-04-23 10:24:10 -0300

    ajustes MVT

--- projeto/apps/urls.py
+++ projeto/apps/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('favoritos/', views.lista_favoritos, name='favoritos'),
     path('favoritar/<int:cafe_id>', views.favoritar, name='favoritar'),
-    path('login/', auth_views.LoginView.as_view(template_name='apps/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('cafeteria/<int:cafe_id>/', perfil_cafeteria, name='perfil-cafeteria'),
     path('cafeteria/<int:cafe_id>/whatsapp/', enviar_whatsapp, name='enviar_whatsapp'),
     path('cafeteria/<int:cafe_id>/enviar-email/', enviar_email, name='enviar-email'),
