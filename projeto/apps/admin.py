8e9426bf78c6de46e0f0fb400befbcfa7bb53530
lilymtbr <lm@cesar.school>
2024-04-22 22:42:52 -0300

    Ajeitando os models

--- projeto/apps/admin.py
+++ projeto/apps/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Cafe
-from .models import Cadastrar_Usuario
+from .models import UserCliente
 
 admin.site.register(Cafe)
-admin.site.register(Cadastrar_Usuario)
+admin.site.register(UserCliente)
