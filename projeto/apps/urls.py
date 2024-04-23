e1e5a555835957e8ffb53dce5b92568004d8b3f6
Thaís <155742254+aguiarth@users.noreply.github.com>
2024-04-23 11:09:17 -0300

    template home

--- projeto/apps/urls.py
+++ projeto/apps/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-from .views import perfil_cafeteria, enviar_email, enviar_whatsapp, cadastro_cafeteria, user_cadastro
+from .views import *
 
 urlpatterns = [
     path('', views.home, name = 'home'),
