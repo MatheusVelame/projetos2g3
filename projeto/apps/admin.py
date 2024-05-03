55d72660d4936bca56d72d3fe47a4699da3408ab
MatheusVelame <mvp2@cesar.school>
2024-05-03 10:25:24 -0300

    removendo o models não utilizado

--- projeto/apps/admin.py
+++ projeto/apps/admin.py
@@ -3,7 +3,5 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Cafe
-from .models import UserCliente
 
 admin.site.register(Cafe)
-admin.site.register(UserCliente)
