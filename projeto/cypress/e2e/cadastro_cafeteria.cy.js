175be384ee50a30ebd4fdf1f514d8e9bee4b8d31
Manuela Cavalcanti <manucamorim@hotmail.com>
2024-05-24 02:56:14 -0300

    testes cadastro caf. e perfil de usuario

---
+++ projeto/cypress/e2e/cadastro_cafeteria.cy.js
@@ -0,0 +1,419 @@
+describe('test suit cadastrar cafeteria', () => {
+    it('cenario1', () => {
+        cy.visit('/')
+        cy.get('.dropdown > button').trigger('mouseover');
+
+    // Passo 3: Esperar que o dropdown esteja visível
+        cy.get('.dropdown > button').then(($button) => {
+        // Passar o mouse sobre o botão dropdown
+            cy.wrap($button).trigger('mouseover');
+    
+            // Forçar a visibilidade do menu dropdown
+            cy.get('.dropdown-menu').invoke('css', 'display', 'block');
+    
+            // Esperar que o dropdown esteja visível
+            cy.get('.dropdown-menu').should('be.visible');
+    
+            // Passo 3: Selecionar o link de login no dropdown
+            cy.get('.dropdown-menu a[href="/login/"]').then(($link) => {
+            const loginUrl = $link.attr('href');
+            cy.visit(loginUrl);
+            });
+        });
+        
+        cy.get('.btn').click()
+        cy.get('#name').type('Samantha Lins')
+        cy.get('#email').type('samlins12@gmail.com')
+        cy.get('#username').type('samantha_lins')
+        cy.get('#password').type('sam123')
+        cy.get('#confirm_password').type('sam123')
+        cy.get('#is_business').click()
+        cy.get('.criar-conta').click()
+
+        cy.get('.dropdown > button').trigger('mouseover');
+        cy.get('.dropdown > button').then(($button) => {
+
+            cy.wrap($button).trigger('mouseover');
+            cy.get('.dropdown-menu').invoke('css', 'display', 'block');
+            cy.get('.dropdown-menu').should('be.visible');
+            cy.get('.dropdown-menu a[href="/cadastro_cafeteria/"]').then(($link) => {
+            const loginUrl = $link.attr('href');
+            cy.visit(loginUrl);
+            });
+        });
+
+        cy.get('#nome_cafeteria').type('Cantinho do Amor')
+        cy.get('#responsavel').type('Samantha Lins')
+        cy.get('#endereco').type('Rua da Felicidade, número 808, Boa Viagem')
+        cy.get('#descricao').type('Este é um ambiente muito legal no Recife Antigo.')
+        cy.get('#email').type('teste@teste.com')
+        cy.get('#horas_funcionamento').type('12:00 - 23:00')
+        cy.get('#link_redesocial').type('https://www.instagram.com/cantinhodoamor')
+        cy.get('#site_cafeteria').type('https://www.cantinhodoamor.com')
+        cy.get('#cnpj').type('77224361000146')
+        cy.get('#whatsapp').type('5581998613003')
+        cy.get('.cadastrar').click()
+        cy.get('h1').invoke('text').then((text) => {
+            expect(text).to.include('Cadastro concluído com sucesso!');
+          });
+        cy.wait(3000)
+        
+        cy.visit('/admin');
+        // cy.get('#id_username').type('admin')
+        // cy.get('#id_password').type('123')
+        // cy.get('.submit-row > input').click()
+    })
+
+    it('cenario2', () => {
+        
+        cy.visit('/')
+        cy.get('.dropdown > button').trigger('mouseover');
+
+        cy.get('.dropdown > button').then(($button) => {
+            cy.wrap($button).trigger('mouseover');
+    
+            cy.get('.dropdown-menu').invoke('css', 'display', 'block');
+    
+            cy.get('.dropdown-menu').should('be.visible');
+    
+            cy.get('.dropdown-menu a[href="/login/"]').then(($link) => {
+            const loginUrl = $link.attr('href');
+            cy.visit(loginUrl);
+            });
+        });
+        
+        cy.get('.btn').click()
+        cy.get('#name').type('Samantha Lins')
+        cy.get('#email').type('samlins12@gmail.com')
+        cy.get('#username').type('samantha_lins')
+        cy.get('#password').type('sam123')
+        cy.get('#confirm_password').type('sam123')
+        cy.get('#is_business').click()
+        cy.get('.criar-conta').click()
+
+        cy.get('.dropdown > button').trigger('mouseover');
+        cy.get('.dropdown > button').then(($button) => {
+
+            cy.wrap($button).trigger('mouseover');
+            cy.get('.dropdown-menu').invoke('css', 'display', 'block');
+            cy.get('.dropdown-menu').should('be.visible');
+            cy.get('.dropdown-menu a[href="/cadastro_cafeteria/"]').then(($link) => {
+            const loginUrl = $link.attr('href');
+            cy.visit(loginUrl);
+            });
+        });
+
+        cy.get('#nome_cafeteria').type('Cantinho do Amor')
+        cy.get('#responsavel').type('Samantha Lins')
+        cy.get('#endereco').type('Rua da Felicidade, número 808, Boa Viagem')
+        cy.get('#descricao').type('Este é um ambiente muito legal no Recife Antigo.')
+        cy.get('#email').type('teste@teste.com')
+        cy.get('#horas_funcionamento').type('12:00 - 23:00')
+        cy.get('#link_redesocial').type('https://www.instagram.com/cantinhodoamor')
+        cy.get('#site_cafeteria').type('https://www.cantinhodoamor.com')
+        cy.get('#cnpj').type('77224361000146')
+        cy.get('#whatsapp').type('5581998613003')
+        cy.get('.cadastrar').click()
+
+        cy.get('.dropdown > button').then(($button) => {
+                cy.wrap($button).trigger('mouseover');
+        
+                cy.get('.dropdown-menu').invoke('css', 'display', 'block');
+        
+                cy.get('.dropdown-menu').should('be.visible');
+        
+                cy.get('.dropdown-menu button[type="submit"]').click(); 
+            });
+        
+        cy.get('.dropdown > button').then(($button) => {
+                cy.wrap($button).trigger('mouseover');
+        
+                cy.get('.dropdown-menu').invoke('css', 'display', 'block');
+        
+                cy.get('.dropdown-menu').should('be.visible');
+
+                cy.get('.dropdown-menu a[href="/login/"]').then(($link) => {
+                const loginUrl = $link.attr('href');
+                cy.visit(loginUrl);
+                });
+            });
+
+            cy.get('.btn').click()
+            cy.get('#name').type('Rosália Brito')
+            cy.get('#email').type('rosaliabrito34@gmail.com')
+            cy.get('#username').type('rosaliab')
+            cy.get('#password').type('rosalia123')
+            cy.get('#confirm_password').type('rosalia123')
+            cy.get('#is_business').click()
+            cy.get('.criar-conta').click()
+    
+            cy.get('.dropdown > button').trigger('mouseover');
+            cy.get('.dropdown > button').then(($button) => {
+    
+                cy.wrap($button).trigger('mouseover');
+                cy.get('.dropdown-menu').invoke('css', 'display', 'block');
+                cy.get('.dropdown-menu').should('be.visible');
+                cy.get('.dropdown-menu a[href="/cadastro_cafeteria/"]').then(($link) => {
+                const loginUrl = $link.attr('href');
+                cy.visit(loginUrl);
+                });
+            });
+    
+            cy.get('#nome_cafeteria').type('Comida de Vó')
+            cy.get('#responsavel').type('Rosália Brito')
+            cy.get('#endereco').type('Rua das Ladeiras, número 500, Boa Vista')
+            cy.get('#descricao').type('Ambiente acolhedor e descontraído')
+            cy.get('#email').type('teste2@teste.com')
+            cy.get('#horas_funcionamento').type('8:00 - 22:00')
+            cy.get('#link_redesocial').type('https://www.instagram.com/comidadevo')
+            cy.get('#site_cafeteria').type('https://www.comidadevo.com')
+            cy.get('#cnpj').type('77224361000146')
+            cy.get('#whatsapp').type('5581981710026')
+            cy.get('.cadastrar').click()
+
+            cy.get('p').invoke('text').then((text) => {
+                expect(text).to.include('O CNPJ já está em uso');
+              });
+            
+            cy.wait(3000)
+
+            //LEMBRAR DE APAGAR NO BANCO DE DADOS
+    })
+
+    it('cenario3', () => {
+        
+        cy.visit('/')
+        cy.get('.dropdown > button').trigger('mouseover');
+
+  
+        cy.get('.dropdown > button').then(($button) => {
+        
+            cy.wrap($button).trigger('mouseover');
+
+            cy.get('.dropdown-menu').invoke('css', 'display', 'block');
+
+            cy.get('.dropdown-menu').should('be.visible');
+    
+            cy.get('.dropdown-menu a[href="/login/"]').then(($link) => {
+            const loginUrl = $link.attr('href');
+            cy.visit(loginUrl);
+            });
+        });
+        
+        cy.get('.btn').click()
+        cy.get('#name').type('Samantha Lins')
+        cy.get('#email').type('samlins12@gmail.com')
+        cy.get('#username').type('samantha_lins')
+        cy.get('#password').type('sam123')
+        cy.get('#confirm_password').type('sam123')
+        cy.get('#is_business').click()
+        cy.get('.criar-conta').click()
+
+        cy.get('.dropdown > button').trigger('mouseover');
+        cy.get('.dropdown > button').then(($button) => {
+
+            cy.wrap($button).trigger('mouseover');
+            cy.get('.dropdown-menu').invoke('css', 'display', 'block');
+            cy.get('.dropdown-menu').should('be.visible');
+            cy.get('.dropdown-menu a[href="/cadastro_cafeteria/"]').then(($link) => {
+            const loginUrl = $link.attr('href');
+            cy.visit(loginUrl);
+            });
+        });
+
+        cy.get('#nome_cafeteria').type('Cantinho do Amor')
+        cy.get('#responsavel').type('Samantha Lins')
+        cy.get('#endereco').type('Rua da Felicidade, número 808, Boa Viagem')
+        cy.get('#descricao').type('Este é um ambiente muito legal no Recife Antigo.')
+        cy.get('#email').type('teste@teste.com')
+        cy.get('#horas_funcionamento').type('12:00 - 23:00')
+        cy.get('#link_redesocial').type('https://www.instagram.com/cantinhodoamor')
+        cy.get('#site_cafeteria').type('https://www.cantinhodoamor.com')
+        cy.get('#cnpj').type('77224361000146')
+        cy.get('#whatsapp').type('5581998613003')
+        cy.get('.cadastrar').click()
+
+        cy.get('.dropdown > button').then(($button) => {
+
+                cy.wrap($button).trigger('mouseover');
+        
+ 
+                cy.get('.dropdown-menu').invoke('css', 'display', 'block');
+
+                cy.get('.dropdown-menu').should('be.visible');
+        
+
+                cy.get('.dropdown-menu button[type="submit"]').click(); 
+            });
+        
+        cy.get('.dropdown > button').then(($button) => {
+                cy.wrap($button).trigger('mouseover');
+        
+                cy.get('.dropdown-menu').invoke('css', 'display', 'block');
+
+                cy.get('.dropdown-menu').should('be.visible');
+        
+                cy.get('.dropdown-menu a[href="/login/"]').then(($link) => {
+                const loginUrl = $link.attr('href');
+                cy.visit(loginUrl);
+                });
+            });
+
+            cy.get('.btn').click()
+            cy.get('#name').type('Júnior Gonçalves')
+            cy.get('#email').type('juniorgoncalves@gmail.com')
+            cy.get('#username').type('juniorgoncalves')
+            cy.get('#password').type('junior1234')
+            cy.get('#confirm_password').type('junior1234')
+            cy.get('#is_business').click()
+            cy.get('.criar-conta').click()
+    
+            cy.get('.dropdown > button').trigger('mouseover');
+            cy.get('.dropdown > button').then(($button) => {
+    
+                cy.wrap($button).trigger('mouseover');
+                cy.get('.dropdown-menu').invoke('css', 'display', 'block');
+                cy.get('.dropdown-menu').should('be.visible');
+                cy.get('.dropdown-menu a[href="/cadastro_cafeteria/"]').then(($link) => {
+                const loginUrl = $link.attr('href');
+                cy.visit(loginUrl);
+                });
+            });
+    
+            cy.get('#nome_cafeteria').type('Love Coffee')
+            cy.get('#responsavel').type('Júnior Golçalves')
+            cy.get('#endereco').type('Rua das Marias, número 230, Boa vista')
+            cy.get('#descricao').type('Ambiente com comidas e cafés deliciosos. Também é coworking e petfriendly.')
+            cy.get('#email').type('teste4@teste.com')
+            cy.get('#horas_funcionamento').type('8:00 - 22:00')
+            cy.get('#link_redesocial').type('https://www.instagram.com/lovecoffee')
+            cy.get('#site_cafeteria').type('https://www.lovecoffee.com')
+            cy.get('#cnpj').type('12333432123489')
+            cy.get('#whatsapp').type('5581998613003')
+            cy.get('.cadastrar').click()
+
+            cy.get('p').invoke('text').then((text) => {
+                expect(text).to.include('O número de WhatsApp já está em uso.');
+              });
+            
+            cy.wait(3000)
+
+            //LEMBRAR DE APAGAR NO BANCO DE DADOS
+    })
+
+    it('cenario4', () => {
+        
+        cy.visit('/')
+        cy.get('.dropdown > button').trigger('mouseover');
+
+
+        cy.get('.dropdown > button').then(($button) => {
+            cy.wrap($button).trigger('mouseover');
+    
+            cy.get('.dropdown-menu').invoke('css', 'display', 'block');
+    
+            cy.get('.dropdown-menu').should('be.visible');
+
+            cy.get('.dropdown-menu a[href="/login/"]').then(($link) => {
+            const loginUrl = $link.attr('href');
+            cy.visit(loginUrl);
+            });
+        });
+        
+        cy.get('.btn').click()
+        cy.get('#name').type('Paolla Oliveira')
+        cy.get('#email').type('paollaoliv@gmail.com')
+        cy.get('#username').type('paollaoliveira')
+        cy.get('#password').type('paolla123')
+        cy.get('#confirm_password').type('paolla123')
+        cy.get('#is_business').click()
+        cy.get('.criar-conta').click()
+
+        cy.get('.dropdown > button').trigger('mouseover');
+        cy.get('.dropdown > button').then(($button) => {
+
+            cy.wrap($button).trigger('mouseover');
+            cy.get('.dropdown-menu').invoke('css', 'display', 'block');
+            cy.get('.dropdown-menu').should('be.visible');
+            cy.get('.dropdown-menu a[href="/cadastro_cafeteria/"]').then(($link) => {
+            const loginUrl = $link.attr('href');
+            cy.visit(loginUrl);
+            });
+        });
+
+        cy.get('#nome_cafeteria').type('Versado')
+        cy.get('#responsavel').type('Paolla Oliveira')
+        cy.get('#endereco').type('Rua Padre Landim, número 302, Recife Antigo')
+        cy.get('#descricao').type('Ambiente confortável para um bom café e bate papo com amigos!')
+        cy.get('#email').type('teste3@teste.com')
+        cy.get('#horas_funcionamento').type('9:00 - 18:00')
+        cy.get('#link_redesocial').type('https://www.instagram.com/versadocafe')
+        cy.get('#site_cafeteria').type('https://www.versadocafe.com')
+        cy.get('#cnpj').type('5576634441000107')
+        cy.get('#whatsapp').type('81940028922')
+        cy.get('.cadastrar').click()
+        cy.get('p').invoke('text').then((text) => {
+            expect(text).to.include('O número de WhatsApp deve ter 13 dígitos.');
+            });
+        
+        cy.wait(3000)
+    })
+
+    it('cenario5', () => {
+        
+        cy.visit('/')
+        cy.get('.dropdown > button').trigger('mouseover');
+
+
+        cy.get('.dropdown > button').then(($button) => {
+            cy.wrap($button).trigger('mouseover');
+    
+            cy.get('.dropdown-menu').invoke('css', 'display', 'block');
+    
+            cy.get('.dropdown-menu').should('be.visible');
+
+            cy.get('.dropdown-menu a[href="/login/"]').then(($link) => {
+            const loginUrl = $link.attr('href');
+            cy.visit(loginUrl);
+            });
+        });
+        
+        cy.get('.btn').click()
+        cy.get('#name').type('Lucas Silva')
+        cy.get('#email').type('lucassilva@gmail.com')
+        cy.get('#username').type('lucas_silva')
+        cy.get('#password').type('lucas123')
+        cy.get('#confirm_password').type('lucas123')
+        cy.get('#is_business').click()
+        cy.get('.criar-conta').click()
+
+        cy.get('.dropdown > button').trigger('mouseover');
+        cy.get('.dropdown > button').then(($button) => {
+
+            cy.wrap($button).trigger('mouseover');
+            cy.get('.dropdown-menu').invoke('css', 'display', 'block');
+            cy.get('.dropdown-menu').should('be.visible');
+            cy.get('.dropdown-menu a[href="/cadastro_cafeteria/"]').then(($link) => {
+            const loginUrl = $link.attr('href');
+            cy.visit(loginUrl);
+            });
+        });
+
+        cy.get('#nome_cafeteria').type('Café do Lucas')
+        cy.get('#responsavel').type('Lucas Silva')
+        cy.get('#endereco').type('Rua das Flores, número 100, Centro')
+        cy.get('#descricao').type('Ambiente confortável para um bom café e bate papo com amigos!')
+        cy.get('#email').type('teste4@teste.com')
+        cy.get('#horas_funcionamento').type('10:00 - 19:00')
+        cy.get('#link_redesocial').type('https://www.instagram.com/cafedolucas')
+        cy.get('#site_cafeteria').type('https://www.cafedolucas.com')
+        cy.get('#cnpj').type('12.345.678/9012-34')
+        cy.get('#whatsapp').type('5581940028933')
+        cy.get('.cadastrar').click()
+        cy.get('p').invoke('text').then((text) => {
+            expect(text).to.include('O CNPJ deve conter apenas números e ter 14 dígitos.');
+            });
+        
+        cy.wait(3000)
+    })
+})
