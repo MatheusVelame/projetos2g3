047c177157b6daaf355963c188f690cb6ecdb6b2
lilymtbr <lm@cesar.school>
2024-05-31 15:08:49 -0300

    Adc no admin

--- projeto/apps/admin.py
+++ projeto/apps/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
+from .models import UserCliente, Cafe, Favorito, Historico, ReservaCafe, Avaliacao
 
-# Register your models here.
+# Função para registrar um modelo se não estiver registrado
+def register_model(admin_site, model):
+    if model not in admin_site._registry:
+        admin_site.register(model)
+    else:
+        print(f"O modelo {model.__name__} já está registrado.")
 
-from .models import Cafe
-
-admin.site.register(Cafe)
+# Registrando os modelos
+register_model(admin.site, UserCliente)
+register_model(admin.site, Cafe)
+register_model(admin.site, Favorito)
+register_model(admin.site, Historico)
+register_model(admin.site, ReservaCafe)
+register_model(admin.site, Avaliacao)
